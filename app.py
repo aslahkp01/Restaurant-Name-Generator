b80c4f073c19594e7916dde8bbabee0cbe2b4e2e
aslahkp01 <aslahkp007@gmail.com>
2026-02-21 14:00:45 +0530

    Updated app UI

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 import streamlit as st
 import langchain_helper
 
-st.title("Restaurant Name Generator")
+st.title("🍽️ Restaurant Name Generator")
 
-cuisine = st.sidebar.selectbox(
-    "Pick a Cuisine",
-    ("Indian", "Italian", "Mexican", "American", "Arabic")
-)
+cuisine_options = ["Indian", "Italian", "Mexican", "American", "Arabic", "Other"]
+
+selected_option = st.sidebar.selectbox("Pick a Cuisine", cuisine_options)
+
+if selected_option == "Other":
+    cuisine = st.sidebar.text_input("Enter your custom cuisine")
+else:
+    cuisine = selected_option
 
 if cuisine:
-    response = langchain_helper.generate_restaurant_name_and_items(cuisine)
+    with st.spinner("Generating ideas..."):
+        response = langchain_helper.generate_restaurant_name_and_items(cuisine)
 
     st.header(response['restaurant_name'])
 
     menu_items = response['menu_items'].split(",")
 
-    st.write("**Menu Items**")
-
+    st.write("### Menu Items")
     for item in menu_items:
         st.write("-", item.strip())
